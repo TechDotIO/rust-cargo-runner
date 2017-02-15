e5c9cb0a0c2624dc579a97fa2c1edaa916b4af2c
Valentin Vetter <valentin@codingame.com>
2017-02-15 10:48:18 +0100

    Reduce rsync verbosity

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Copy cargo registry
 [ -d /project/target/.cargo ] && \
-	rsync -av /project/target/.cargo/ ~/.cargo
+	rsync -a /project/target/.cargo/ ~/.cargo
 
 # Copy answer code
 cp -a /project/answer/* /project/target/source/
