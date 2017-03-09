49965c7aa4ffebf125e762c85827c52dd6671ab5
Valentin Vetter <valentin@codingame.com>
2017-03-09 15:12:32 +0100

    Don't copy the `/project/answer` directory
    Due to a change in the CodinGame build process, the user's answer code is now copied into /project/target by default.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Copy answer code
-cp -a /project/answer/* /project/target
-
 cd /project/target
 
 # Run the given testcase(s)
-cargo build -q && cargo test -q --lib $@
+cargo build -q \
+    && cargo test -q --lib $@
